3e5e3f705b21a653b4d4c723c7477bdc4ad16022
nadi-nikoko <149614105+nadi-nikoko@users.noreply.github.com>
2024-04-10 17:05:51 +0200

    files of security and thymeleaf

---
+++ spring-boot/thymeleaf/01-thymeleaf-hello-world/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class DemoController {
+    @GetMapping("/hello")
+    public String sayHello(Model theModel){
+        theModel.addAttribute("theDate", LocalDateTime.now());
+        return "helloworld";
+    }
+}
